072de90230906a3e5d552cf942346b210f90251e
Athira.P.S <148539612+AthiraPromod@users.noreply.github.com>
2026-01-07 09:04:12 +0530

    Add files via upload

---
+++ flask-login-system-main/flaskr/__init__.py
@@ -0,0 +1,46 @@
+import os
+from flask import Flask, redirect, url_for, render_template, session
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    # CONFIGURATION
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # Load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # Load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def index():
+        return redirect(url_for('auth.login'))
+
+    @app.route('/dashboard')
+    def dashboard():
+        if 'user_id' not in session:
+            return redirect(url_for('auth.login'))
+        return render_template('dashboard.html')
+    
+    @app.route('/hello')
+    def hello():
+        return 'Hello, world'
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    return app
